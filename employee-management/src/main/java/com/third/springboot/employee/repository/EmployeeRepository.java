e302db479354ae42e730767321a3559c76c8d1e2
Swaroop Reddy <152079457+Swaroopreddy-m@users.noreply.github.com>
2026-01-24 00:54:37 +0530

    Delete employee-management directory

--- employee-management/src/main/java/com/third/springboot/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.third.springboot.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.third.springboot.employee.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
